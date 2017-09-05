fd5d72cbf6d173363420a4c2306979d038cac648
Ian Shipman <ics@gambolingpangolin.com>
2017-09-05 16:04:45 -0500

    ZeroFrame.Ports - docs

--- src/ZeroFrame/Ports.elm
+++ src/ZeroFrame/Ports.elm
@@ -1,8 +1,14 @@
 port module ZeroFrame.Ports exposing (
-  RawWrapperMessage
+  RawZeroFrameMessage
 	, sendZeroFrameMsg
 	, recvZeroFrameMsg
 	) 
+
+{-| This module contains the two ports needed to interact with the ZeroFrame wrapper.  End-users should use 'ZeroFrame.Core.command' and 'ZeroFrame.Core.response' to interact with the wrapper.
+
+# Ports
+@docs sendZeroFrameMsg, recvZeroFrameMsg
+-}
 
 type alias RawWrapperMessage =
   { id : Int
@@ -11,5 +17,10 @@ type alias RawWrapperMessage =
   , to : Maybe Int
   , result : Value }
 
+{-| Send a message out to the wrapper.  
+-}
 port sendZeroFrameMsg : RawWrapperMessage -> Cmd msg 
+
+{-| Receive a message from the wrapper.
+-}
 port recvZeroFrameMsg : (RawWrapperMessage -> msg) -> Sub msg
